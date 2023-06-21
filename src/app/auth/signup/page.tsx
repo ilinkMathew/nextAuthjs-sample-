db6872b06f338d2d16a1c8239a6967ee1c16065c
Nikil Mathew <ilinkmat@Nikils-MacBook-Pro.local>
2023-06-21 23:04:59 +0100

    Install nextjs & setup auth

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -37,11 +37,6 @@ export default function SignUp() {
             <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
               Password
             </label>
-            <div className="text-sm">
-              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                Forgot password?
-              </a>
-            </div>
           </div>
           <div className="mt-2">
             <input
@@ -54,7 +49,23 @@ export default function SignUp() {
             />
           </div>
         </div>
-
+        <div>
+          <div className="flex items-center justify-between">
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+              Confirm Password
+            </label>
+          </div>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
         <div>
           <button
             type="submit"
@@ -65,12 +76,7 @@ export default function SignUp() {
         </div>
       </form>
 
-      <p className="mt-10 text-center text-sm text-gray-500">
-        Not a member?{' '}
-        <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-          Start a 14 day free trial
-        </a>
-      </p>
+
     </div>
   </div>
   )
